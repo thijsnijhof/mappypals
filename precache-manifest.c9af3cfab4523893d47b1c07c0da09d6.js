7ff059da87ffe9e33cd1d8963c239354102b6aae
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-01 21:45:36 +0000

    Deploy zero-to-mastery/mappypals to github.com/zero-to-mastery/mappypals.git:gh-pages

---
+++ precache-manifest.c9af3cfab4523893d47b1c07c0da09d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b7015be3440e936bd27",
+    "url": "/mappypals/static/css/main.63a112d1.chunk.css"
+  },
+  {
+    "revision": "0b7015be3440e936bd27",
+    "url": "/mappypals/static/js/main.7f4085fd.chunk.js"
+  },
+  {
+    "revision": "c57707e0e7c42496a68c",
+    "url": "/mappypals/static/js/runtime~main.11b2500f.js"
+  },
+  {
+    "revision": "c0cc5c5a7a77181d9693",
+    "url": "/mappypals/static/js/2.9a0e02b8.chunk.js"
+  },
+  {
+    "revision": "d96e6a53013e7a91058441e68ffa0e52",
+    "url": "/mappypals/static/media/adult-backlit-beach.d96e6a53.jpg"
+  },
+  {
+    "revision": "6203924a123a7b560b9ea92b98069ab7",
+    "url": "/mappypals/static/media/mappyPals.6203924a.svg"
+  },
+  {
+    "revision": "9ff0000d12d89426a4ae8c5fedf7861f",
+    "url": "/mappypals/static/media/LogoInverted.9ff0000d.svg"
+  },
+  {
+    "revision": "c3ccea7b6f489d09bde495a857e9b894",
+    "url": "/mappypals/index.html"
+  }
+];
